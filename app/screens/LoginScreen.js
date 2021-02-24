ca3dccc0f86aa18be9026c64baf79a372fccdbf0
barbozadevin <devin.barboza12@gmail.com>
2021-02-24 16:27:50 +0530

    Added initial redux code

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,91 +1,112 @@
-import React, { Component, useState } from 'react';
-import { View, StyleSheet,Text, Image } from 'react-native';
-import { TextInput,Button } from 'react-native-paper';
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, Image} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
 import {useSelector, useDispatch} from 'react-redux';
-import {signIn} from '../actions/Actions'
+//import {signIn} from '../actions/Actions'
 
+import {authService} from '../services/auth.service';
+import Connector from '../utils/Connector';
+import PropTypes from 'prop-types';
+import store from '../utils/store';
 
+function LoginScreen({actions}) {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
 
-function LoginScreen(){
-
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const dispatch = useDispatch();
-          
-    return (
-      <View style={styles.main}>
-        <Image source={require('../assets/logo.png')} style={styles.img} />
-          <View style={styles.login}>
-          <Text style={styles.loginText}>
-              Login
-          </Text>
-            <TextInput
-                label="Username"
-                value={userName}
-                onChangeText={text => setUserName(text)}
-                foc
-            />
-            <TextInput
-                style={styles.textin}
-                label="Password"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                foc
-                secureTextEntry={true}
-            />
-
-            <Button style={styles.submit} contentStyle={{height: 50}} mode="contained" onPress={() => dispatch(signIn({userName, password}))}>
-                Submit
-            </Button>
-
-            <View style={styles.signupText} >
-                <Text>
-                    Don't have account? <Text onPress={()=>this.props.navigation.navigate('SignupScreen')}>Sign Up</Text>
-                </Text>
-            </View>
-        </View>
-      </View>
-    );
+  function login() {
+    authService.login(userName, password).then((me) => {
+      actions.saveMe(me);
+      console.log(store.getState().auth);
+      actions.authenticate();
+      //history.push('/dashboard');
+    });
   }
 
+  return (
+    <View style={styles.main}>
+      <Image source={require('../assets/logo.png')} style={styles.img} />
+      <View style={styles.login}>
+        <Text style={styles.loginText}>Login</Text>
+        <TextInput
+          label="Username"
+          value={userName}
+          onChangeText={(text) => setUserName(text)}
+          foc
+        />
+        <TextInput
+          style={styles.textin}
+          label="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          foc
+          secureTextEntry={true}
+        />
+
+        <Button
+          style={styles.submit}
+          contentStyle={{height: 50}}
+          mode="contained"
+          onPress={() => login()}>
+          Submit
+        </Button>
+
+        <View style={styles.signupText}>
+          <Text>
+            Don't have account?{' '}
+            <Text
+              onPress={() => this.props.navigation.navigate('SignupScreen')}>
+              Sign Up
+            </Text>
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const ConnectedLogin = (props) => (
+  <Connector>
+    {({actions}) => <LoginScreen actions={actions.auth} {...props} />}
+  </Connector>
+);
 
 const styles = StyleSheet.create({
-    img:{
-        alignSelf:'center',
-        margin: 20
-    },
-    main:{
-        flex: 1,
-        
-    },
-    mainText:{
-        margin: "10%",
-        justifyContent:"center",
-        alignSelf:"center",
-        alignContent: 'center',
-        fontSize: 30,
-    },
-    textin:{
-        marginTop: 10,
-    },
-    login:{
-        flex: 0.75,
-        width: "85%",
-        justifyContent:"center",
-        alignSelf:"center",
-    },
-    submit:{
-        marginTop: 10,
-    },
-    loginText:{
-        fontSize: 20,
-        margin: 20,
-        alignSelf: "center"
-    },
-    signupText:{
-        alignItems: "center",
-        marginTop: 10,
-    }
-})
+  img: {
+    alignSelf: 'center',
+    margin: 20,
+  },
+  main: {
+    flex: 1,
+  },
+  mainText: {
+    margin: '10%',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignContent: 'center',
+    fontSize: 30,
+  },
+  textin: {
+    marginTop: 10,
+  },
+  login: {
+    flex: 0.75,
+    width: '85%',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  submit: {
+    marginTop: 10,
+  },
+  loginText: {
+    fontSize: 20,
+    margin: 20,
+    alignSelf: 'center',
+  },
+  signupText: {
+    alignItems: 'center',
+    marginTop: 10,
+  },
+});
 
-export default LoginScreen;
+export default ConnectedLogin;
---
+++ app/services/auth.service.js
@@ -0,0 +1,30 @@
+import Constants from '../utils/Constants';
+import Me from '../utils/Me';
+import HttpHelper from '../utils/HttpHelperUtil';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const {Routes} = Constants.Urls.apis;
+
+function getUserPayload(user) {
+  return JSON.stringify(user);
+}
+
+async function handleAuthenticate(me) {
+  if (!!me) {
+    await AsyncStorage.setItem('me', JSON.stringify(me));
+  }
+  return me;
+}
+
+function login(name, password) {
+  const payload = getUserPayload({name, password});
+  console.log(payload);
+
+  return HttpHelper.postWithoutAuth(Routes.LOGIN, payload).then(
+    handleAuthenticate,
+  );
+}
+
+export const authService = {
+  login,
+};
